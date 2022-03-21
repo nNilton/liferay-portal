99c95cb0145957a335ae1cea5afc53670c23a68f
Nilton Vieira <nilton.vieira@liferay.com>
2022-03-21 09:09:53 -0300

    LPS-147554 add thread to process all Testray Cases

--- modules/apps/site-initializer/site-initializer-testray/extra/java-function/src/main/java/com/liferay/site/initializer/testray/extra/java/function/ImportResults.java
+++ modules/apps/site-initializer/site-initializer-testray/extra/java-function/src/main/java/com/liferay/site/initializer/testray/extra/java/function/ImportResults.java
@@ -146,9 +146,17 @@ public class ImportResults {
 			Map<String, Object> testrayCasePropertiesMap =
 				_getTestrayCaseProperties((Element)testcaseNode);
 
-			_addTestrayCase(
-				testcaseNode, testrayBuildId, testrayProjectId, testrayRunId,
-				testrayCasePropertiesMap);
+			new Thread(){
+				public void run(){
+					try{
+						_addTestrayCase(
+							testcaseNode, testrayBuildId, testrayProjectId,
+							testrayRunId, testrayCasePropertiesMap);
+					}catch(Exception e){
+						e.printStackTrace();
+					}
+				}
+			}.start();
 		}
 	}
 
