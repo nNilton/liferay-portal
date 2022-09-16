e945ec4552fbc986cd1e162ef6a2c6fa56860d7a
Nilton Vieira <nilton.vieira@liferay.com>
2022-09-16 15:02:41 -0300

    LPS-163634 remove moved methods

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/segments/SegmentsExperienceUtil.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/segments/SegmentsExperienceUtil.java
@@ -14,53 +14,26 @@
 
 package com.liferay.layout.content.page.editor.web.internal.segments;
 
-import com.liferay.counter.kernel.service.CounterLocalServiceUtil;
-import com.liferay.fragment.model.FragmentEntryLink;
-import com.liferay.fragment.processor.PortletRegistry;
-import com.liferay.fragment.service.FragmentEntryLinkLocalServiceUtil;
-import com.liferay.layout.content.page.editor.web.internal.util.layout.structure.LayoutStructureUtil;
-import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalServiceUtil;
-import com.liferay.layout.util.structure.FragmentStyledLayoutStructureItem;
-import com.liferay.layout.util.structure.LayoutStructure;
-import com.liferay.portal.kernel.comment.CommentManager;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.json.JSONException;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Layout;
-import com.liferay.portal.kernel.model.Portlet;
-import com.liferay.portal.kernel.model.PortletPreferences;
-import com.liferay.portal.kernel.portlet.PortletIdCodec;
-import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
-import com.liferay.portal.kernel.service.PortletLocalServiceUtil;
-import com.liferay.portal.kernel.service.PortletPreferenceValueLocalServiceUtil;
-import com.liferay.portal.kernel.service.PortletPreferencesLocalServiceUtil;
-import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.CopyLayoutThreadLocal;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.HttpComponentsUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
-import com.liferay.portal.kernel.util.PortletKeys;
-import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.portal.kernel.uuid.PortalUUIDUtil;
 import com.liferay.segments.constants.SegmentsExperimentConstants;
 import com.liferay.segments.model.SegmentsExperience;
 import com.liferay.segments.model.SegmentsExperiment;
 import com.liferay.segments.service.SegmentsExperienceServiceUtil;
 import com.liferay.segments.service.SegmentsExperimentLocalServiceUtil;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.function.Function;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -69,32 +42,6 @@ import javax.servlet.http.HttpServletRequest;
  * @author David Arques
  */
 public class SegmentsExperienceUtil {
-
-	public static void copySegmentsExperienceData(
-			long plid, CommentManager commentManager, long groupId,
-			PortletRegistry portletRegistry, long sourceSegmentsExperienceId,
-			long targetSegmentsExperienceId,
-			Function<String, ServiceContext> serviceContextFunction,
-			long userId)
-		throws PortalException {
-
-		boolean copyLayout = CopyLayoutThreadLocal.isCopyLayout();
-
-		try {
-			CopyLayoutThreadLocal.setCopyLayout(true);
-
-			_copyLayoutData(
-				plid, commentManager, groupId, portletRegistry,
-				sourceSegmentsExperienceId, targetSegmentsExperienceId,
-				serviceContextFunction, userId);
-		}
-		catch (Throwable throwable) {
-			throw new PortalException(throwable);
-		}
-		finally {
-			CopyLayoutThreadLocal.setCopyLayout(copyLayout);
-		}
-	}
 
 	public static Map<String, Object> getAvailableSegmentsExperiences(
 			HttpServletRequest httpServletRequest)
@@ -192,139 +139,6 @@ public class SegmentsExperienceUtil {
 		).build();
 	}
 
-	private static void _copyLayoutData(
-			long plid, CommentManager commentManager, long groupId,
-			PortletRegistry portletRegistry, long sourceSegmentsExperienceId,
-			long targetSegmentsExperienceId,
-			Function<String, ServiceContext> serviceContextFunction,
-			long userId)
-		throws PortalException {
-
-		LayoutStructure layoutStructure =
-			LayoutStructureUtil.getLayoutStructure(
-				groupId, plid, sourceSegmentsExperienceId);
-
-		JSONObject dataJSONObject = _updateLayoutDataJSONObject(
-			plid, commentManager, layoutStructure, groupId, portletRegistry,
-			sourceSegmentsExperienceId, serviceContextFunction,
-			targetSegmentsExperienceId, userId);
-
-		LayoutPageTemplateStructureLocalServiceUtil.
-			updateLayoutPageTemplateStructureData(
-				groupId, plid, targetSegmentsExperienceId,
-				dataJSONObject.toString());
-	}
-
-	private static void _copyPortletPreferences(
-		FragmentEntryLink fragmentEntryLink,
-		FragmentEntryLink newFragmentEntryLink, long plid,
-		PortletRegistry portletRegistry) {
-
-		for (String portletId :
-				portletRegistry.getFragmentEntryLinkPortletIds(
-					fragmentEntryLink)) {
-
-			_getNewPortletPreferencesOptional(
-				fragmentEntryLink.getNamespace(),
-				newFragmentEntryLink.getNamespace(), plid, portletId);
-		}
-	}
-
-	private static String _getNewEditableValues(
-			String editableValues, String namespace, String newNamespace,
-			long plid)
-		throws JSONException {
-
-		JSONObject editableValuesJSONObject = JSONFactoryUtil.createJSONObject(
-			editableValues);
-
-		String instanceId = editableValuesJSONObject.getString("instanceId");
-		String portletId = editableValuesJSONObject.getString("portletId");
-
-		if (Validator.isNull(instanceId) || Validator.isNull(portletId)) {
-			return editableValues;
-		}
-
-		return _getNewPortletPreferencesOptional(
-			namespace, newNamespace, plid,
-			PortletIdCodec.encode(portletId, instanceId)
-		).map(
-			portletPreferences -> {
-				JSONObject newEditableValuesJSONObject =
-					editableValuesJSONObject.put(
-						"instanceId",
-						PortletIdCodec.decodeInstanceId(
-							portletPreferences.getPortletId()));
-
-				return newEditableValuesJSONObject.toString();
-			}
-		).orElse(
-			editableValues
-		);
-	}
-
-	private static String _getNewPortletId(
-		String namespace, String newNamespace, String portletId) {
-
-		if (!portletId.contains(namespace)) {
-			return PortletIdCodec.encode(
-				PortletIdCodec.decodePortletName(portletId), newNamespace);
-		}
-
-		return StringUtil.replace(portletId, namespace, newNamespace);
-	}
-
-	private static Optional<PortletPreferences>
-		_getNewPortletPreferencesOptional(
-			String namespace, String newNamespace, long plid,
-			String portletId) {
-
-		PortletPreferences portletPreferences =
-			PortletPreferencesLocalServiceUtil.fetchPortletPreferences(
-				PortletKeys.PREFS_OWNER_ID_DEFAULT,
-				PortletKeys.PREFS_OWNER_TYPE_LAYOUT, plid, portletId);
-
-		if (portletPreferences == null) {
-			return Optional.empty();
-		}
-
-		Portlet portlet = PortletLocalServiceUtil.getPortletById(portletId);
-
-		if ((portlet == null) || portlet.isUndeployedPortlet()) {
-			return Optional.empty();
-		}
-
-		String newPortletId = _getNewPortletId(
-			namespace, newNamespace, portletId);
-
-		PortletPreferences existingPortletPreferences =
-			PortletPreferencesLocalServiceUtil.fetchPortletPreferences(
-				portletPreferences.getOwnerId(),
-				portletPreferences.getOwnerType(), plid, newPortletId);
-
-		javax.portlet.PortletPreferences jxPortletPreferences =
-			PortletPreferenceValueLocalServiceUtil.getPreferences(
-				portletPreferences);
-
-		if (existingPortletPreferences == null) {
-			return Optional.of(
-				PortletPreferencesLocalServiceUtil.addPortletPreferences(
-					portletPreferences.getCompanyId(),
-					portletPreferences.getOwnerId(),
-					portletPreferences.getOwnerType(), plid, newPortletId,
-					portlet,
-					PortletPreferencesFactoryUtil.toXML(jxPortletPreferences)));
-		}
-
-		return Optional.of(
-			PortletPreferencesLocalServiceUtil.updatePreferences(
-				existingPortletPreferences.getOwnerId(),
-				existingPortletPreferences.getOwnerType(),
-				existingPortletPreferences.getPlid(),
-				existingPortletPreferences.getPortletId(),
-				jxPortletPreferences));
-	}
-
 	private static Optional<SegmentsExperiment> _getSegmentsExperimentOptional(
 			ThemeDisplay themeDisplay, long segmentsExperienceId)
 		throws Exception {
@@ -350,79 +164,6 @@ public class SegmentsExperienceUtil {
 
 		return HttpComponentsUtil.addParameter(
 			layoutFullURL, "segmentsExperienceId", segmentsExperienceId);
-	}
-
-	private static JSONObject _updateLayoutDataJSONObject(
-			long plid, CommentManager commentManager,
-			LayoutStructure layoutStructure, long groupId,
-			PortletRegistry portletRegistry, long sourceSegmentsExperienceId,
-			Function<String, ServiceContext> serviceContextFunction,
-			long targetSegmentsExperienceId, long userId)
-		throws PortalException {
-
-		List<FragmentEntryLink> fragmentEntryLinks =
-			FragmentEntryLinkLocalServiceUtil.
-				getFragmentEntryLinksBySegmentsExperienceId(
-					groupId, sourceSegmentsExperienceId, plid);
-
-		for (FragmentEntryLink fragmentEntryLink : fragmentEntryLinks) {
-			if (fragmentEntryLink.isDeleted()) {
-				continue;
-			}
-
-			FragmentStyledLayoutStructureItem
-				fragmentStyledLayoutStructureItem =
-					(FragmentStyledLayoutStructureItem)
-						layoutStructure.
-							getLayoutStructureItemByFragmentEntryLinkId(
-								fragmentEntryLink.getFragmentEntryLinkId());
-
-			if (fragmentStyledLayoutStructureItem == null) {
-				continue;
-			}
-
-			FragmentEntryLink newFragmentEntryLink =
-				(FragmentEntryLink)fragmentEntryLink.clone();
-
-			newFragmentEntryLink.setUuid(PortalUUIDUtil.generate());
-			newFragmentEntryLink.setFragmentEntryLinkId(
-				CounterLocalServiceUtil.increment());
-			newFragmentEntryLink.setCreateDate(new Date());
-			newFragmentEntryLink.setModifiedDate(new Date());
-			newFragmentEntryLink.setOriginalFragmentEntryLinkId(
-				fragmentEntryLink.getFragmentEntryLinkId());
-			newFragmentEntryLink.setSegmentsExperienceId(
-				targetSegmentsExperienceId);
-
-			String newNamespace = fragmentEntryLink.getNamespace();
-
-			newFragmentEntryLink.setEditableValues(
-				_getNewEditableValues(
-					fragmentEntryLink.getEditableValues(),
-					fragmentEntryLink.getNamespace(), newNamespace, plid));
-
-			newFragmentEntryLink.setNamespace(newNamespace);
-
-			newFragmentEntryLink.setLastPropagationDate(new Date());
-
-			newFragmentEntryLink =
-				FragmentEntryLinkLocalServiceUtil.addFragmentEntryLink(
-					newFragmentEntryLink);
-
-			fragmentStyledLayoutStructureItem.setFragmentEntryLinkId(
-				newFragmentEntryLink.getFragmentEntryLinkId());
-
-			commentManager.copyDiscussion(
-				userId, groupId, FragmentEntryLink.class.getName(),
-				fragmentEntryLink.getFragmentEntryLinkId(),
-				newFragmentEntryLink.getFragmentEntryLinkId(),
-				serviceContextFunction);
-
-			_copyPortletPreferences(
-				fragmentEntryLink, newFragmentEntryLink, plid, portletRegistry);
-		}
-
-		return layoutStructure.toJSONObject();
 	}
 
 }
