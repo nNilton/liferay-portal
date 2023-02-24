f5c7d36d8e6b781d665b535760b7ff19051ca207
Nilton Vieira <nilton.vieira@liferay.com>
2023-02-24 09:27:59 -0300

    wip

---
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/deploy/auto/SiteInitializerAutoDeployListener.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.initializer.extender.internal.deploy.auto;
+
+import com.liferay.portal.kernel.deploy.auto.AutoDeployException;
+import com.liferay.portal.kernel.deploy.auto.AutoDeployListener;
+import com.liferay.portal.kernel.deploy.auto.AutoDeployer;
+import com.liferay.portal.kernel.deploy.auto.context.AutoDeploymentContext;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
+
+import java.util.zip.ZipFile;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author José Abelenda
+ */
+@Component(service = AutoDeployListener.class)
+public class SiteInitializerAutoDeployListener implements AutoDeployListener {
+
+	@Override
+	public int deploy(AutoDeploymentContext autoDeploymentContext)
+		throws AutoDeployException {
+
+		try (ZipFile zipFile = new ZipFile(autoDeploymentContext.getFile())) {
+			_deploy(zipFile);
+		}
+		catch (Exception exception) {
+			throw new AutoDeployException(exception);
+		}
+
+		return AutoDeployer.CODE_DEFAULT;
+	}
+
+	@Override
+	public boolean isDeployable(AutoDeploymentContext autoDeploymentContext)
+		throws AutoDeployException {
+
+		return true;
+	}
+
+	private void _deploy(ZipFile zipFile) throws Exception {
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				"Deploying site initializer bundle from file " +
+				zipFile.getName());
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		SiteInitializerAutoDeployListener.class);
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private com.liferay.portal.kernel.util.File _file;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+}
