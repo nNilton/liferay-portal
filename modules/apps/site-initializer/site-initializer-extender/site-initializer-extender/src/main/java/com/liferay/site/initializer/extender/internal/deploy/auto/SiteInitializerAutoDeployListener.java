473f81448ce5962cb60a602ee04aaf8dd20eaafa
Nilton Vieira <nilton.vieira@liferay.com>
2023-02-28 16:44:14 -0300

    LPS-???? isDeployable

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/deploy/auto/SiteInitializerAutoDeployListener.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/deploy/auto/SiteInitializerAutoDeployListener.java
@@ -18,17 +18,24 @@ import com.liferay.portal.kernel.deploy.auto.AutoDeployException;
 import com.liferay.portal.kernel.deploy.auto.AutoDeployListener;
 import com.liferay.portal.kernel.deploy.auto.AutoDeployer;
 import com.liferay.portal.kernel.deploy.auto.context.AutoDeploymentContext;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.StringUtil;
-import java.net.URL;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.site.initializer.SiteInitializerFactory;
 
 import java.io.File;
+
 import java.util.Enumeration;
+import java.util.Objects;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 import org.osgi.service.component.annotations.Component;
@@ -67,32 +74,18 @@ public class SiteInitializerAutoDeployListener implements AutoDeployListener {
 		}
 
 		try (ZipFile zipFile = new ZipFile(file)) {
+			Enumeration<? extends ZipEntry> enumeration = zipFile.entries();
 
-			Enumeration<URL> enumeration = zipFile.entries();
+			while (enumeration.hasMoreElements()) {
+				ZipEntry zipEntry = enumeration.nextElement();
 
-
-
-
-
-
-			for (BatchEngineZipUnit batchEngineZipUnit :
-				_getBatchEngineZipUnits(zipFile)) {
-
-				if (!batchEngineZipUnit.isValid()) {
+				if (!zipEntry.isDirectory() || !_isValid(zipEntry.getName())) {
 					continue;
 				}
 
-				BatchEngineImportConfiguration batchEngineImportConfiguration =
-					_getBatchEngineImportConfiguration(batchEngineZipUnit);
+				_setDeployConfiguration(zipFile, zipEntry.getName());
 
-				if ((batchEngineImportConfiguration != null) &&
-					(batchEngineImportConfiguration.companyId > 0) &&
-					(batchEngineImportConfiguration.userId > 0) &&
-					Validator.isNotNull(
-						batchEngineImportConfiguration.className) &&
-					Validator.isNotNull(
-						batchEngineImportConfiguration.version)) {
-
+				if ((_companyId > 0) && (_userId > 0)) {
 					return true;
 				}
 			}
@@ -101,28 +94,82 @@ public class SiteInitializerAutoDeployListener implements AutoDeployListener {
 			throw new AutoDeployException(exception);
 		}
 
-
+		return false;
 	}
 
 	private void _deploy(ZipFile zipFile) throws Exception {
 		if (_log.isInfoEnabled()) {
 			_log.info(
 				"Deploying site initializer bundle from file " +
-				zipFile.getName());
+					zipFile.getName());
+		}
+	}
+
+	private boolean _isValid(String fileName) {
+		if (Objects.equals(fileName, "site-initializer/")) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private void _setDeployConfiguration(ZipFile zipFile, String zipEntryName)
+		throws Exception {
+
+		ZipEntry zipEntry = zipFile.getEntry(zipEntryName + "config.json");
+
+		if (zipEntry != null) {
+			JSONObject jsonObject = _jsonFactory.createJSONObject(
+				StringUtil.read(zipFile.getInputStream(zipEntry)));
+
+			_userId = jsonObject.getLong("userId");
+			_companyId = jsonObject.getLong("companyId");
+		}
+		else {
+			if (_log.isWarnEnabled()) {
+				_log.warn("Using default company ID for this site process");
+			}
+
+			try {
+				Company company = _companyLocalService.getCompanyByWebId(
+					PropsUtil.get(PropsKeys.COMPANY_DEFAULT_WEB_ID));
+
+				_companyId = company.getCompanyId();
+			}
+			catch (PortalException portalException) {
+				_log.error("Unable to get default company ID", portalException);
+			}
+
+			if (_log.isWarnEnabled()) {
+				_log.warn("Using default user ID for this site process");
+			}
+
+			try {
+				_userId = _userLocalService.getUserIdByScreenName(
+					_companyId,
+					PropsUtil.get(PropsKeys.DEFAULT_ADMIN_SCREEN_NAME));
+			}
+			catch (PortalException portalException) {
+				_log.error("Unable to get default user ID", portalException);
+			}
 		}
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		SiteInitializerAutoDeployListener.class);
 
-	@Reference
-	private SiteInitializerFactory _siteInitializerFactory;
+	private long _companyId;
 
 	@Reference
 	private CompanyLocalService _companyLocalService;
 
 	@Reference
 	private com.liferay.portal.kernel.util.File _file;
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+	private long _userId;
 
 	@Reference
 	private UserLocalService _userLocalService;
