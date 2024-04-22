402d23933645003977a96066a775bfb3b15d34cc
Marco Leo <marco.leo@liferay.com>
2024-04-22 10:31:13 -0300

    LPD-23909 Fix Method signature to support the new column

---
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/v9_2_0/ObjectDefinitionUpgradeProcess.java
@@ -0,0 +1,33 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.internal.upgrade.v9_2_0;
+
+import com.liferay.object.model.ObjectDefinitionTable;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.upgrade.UpgradeProcessFactory;
+import com.liferay.portal.kernel.upgrade.UpgradeStep;
+
+/**
+ * @author Selton Guedes
+ */
+public class ObjectDefinitionUpgradeProcess extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		runSQL("update ObjectDefinition set enableIndexedSearch = [$TRUE$]");
+	}
+
+	@Override
+	protected UpgradeStep[] getPreUpgradeSteps() {
+		return new UpgradeStep[] {
+			UpgradeProcessFactory.addColumns(
+				ObjectDefinitionTable.INSTANCE.getTableName(),
+				ObjectDefinitionTable.INSTANCE.enableIndexedSearch.getName() +
+					" BOOLEAN")
+		};
+	}
+
+}
