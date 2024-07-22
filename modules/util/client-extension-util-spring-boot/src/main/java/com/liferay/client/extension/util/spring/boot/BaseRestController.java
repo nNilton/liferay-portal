667762ed37d47b5a423983797f4d1710c2a336ce
Nilton Vieira <nilton.vieira@liferay.com>
2024-07-22 10:27:44 -0300

    LPD-31239 add log to BaseRestController

--- modules/util/client-extension-util-spring-boot/src/main/java/com/liferay/client/extension/util/spring/boot/BaseRestController.java
+++ modules/util/client-extension-util-spring-boot/src/main/java/com/liferay/client/extension/util/spring/boot/BaseRestController.java
@@ -187,6 +187,40 @@ public abstract class BaseRestController {
 		}
 	}
 
+	protected void log(Jwt jwt, Log log) {
+		if (log.isInfoEnabled()) {
+			log.info("JWT Claims: " + jwt.getClaims());
+			log.info("JWT ID: " + jwt.getId());
+			log.info("JWT Subject: " + jwt.getSubject());
+		}
+	}
+
+	protected void log(Jwt jwt, Log log, Map<String, String> parameters) {
+		if (log.isInfoEnabled()) {
+			log.info("JWT Claims: " + jwt.getClaims());
+			log.info("JWT ID: " + jwt.getId());
+			log.info("JWT Subject: " + jwt.getSubject());
+			log.info("Parameters: " + parameters);
+		}
+	}
+
+	protected void log(Jwt jwt, Log log, String json) {
+		if (log.isInfoEnabled()) {
+			try {
+				JSONObject jsonObject = new JSONObject(json);
+
+				log.info("JSON: " + jsonObject.toString(4));
+			}
+			catch (Exception exception) {
+				log.error("JSON: " + json, exception);
+			}
+
+			log.info("JWT Claims: " + jwt.getClaims());
+			log.info("JWT ID: " + jwt.getId());
+			log.info("JWT Subject: " + jwt.getSubject());
+		}
+	}
+
 	protected String patch(String authorization, String body, String path) {
 		return _getWebClient(
 		).patch(
